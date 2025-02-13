59d946f2442b9315a5e31f14e0af111b42b2d73d
Jules Royet <jules.royet@orange.com>
2025-02-13 14:33:53 +0100

    Amélioration des images de projets et ajout du copyright dans le footer

--- src/components/Navigations/Footer/index.tsx
+++ src/components/Navigations/Footer/index.tsx
@@ -1,11 +1,18 @@
 import styles from './footer.module.scss';
 
 export default function Footer() {
-    return (
-        <footer className={styles['footer-container']}>
-            Built with <a href={'https://nextjs.org/'} target={'_blank'}>Next.js</a> and
-            <a href={'https://nextjs.org/docs/app/building-your-application/styling/css-modules'}> SCSS Modules</a>,
-            deployed using <a href={'https://vercel.com/'} target={'_blank'}>Vercel</a>
-        </footer>
-    );
+  const currentYear = new Date().getFullYear(); // Récupérer l'année en cours
+
+  return (
+    <footer className={styles['footer-container']}>
+      <p>
+        Built with <a href={'https://nextjs.org/'} target={'_blank'}>Next.js</a> and
+        <a href={'https://nextjs.org/docs/app/building-your-application/styling/css-modules'}> SCSS Modules</a>,
+        deployed using <a href={'https://vercel.com/'} target={'_blank'}>Vercel</a>
+      </p>
+      <p>
+        &copy; {currentYear} Jules Royet. All rights reserved.
+      </p>
+    </footer>
+  );
 }
